5831f181c4432ab32b2e2afcf48a0785bd9f9f28
Winter LaMon <winter.lamon@gmail.com>
2017-09-27 01:28:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,24 +2,27 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
+def batch_badge_creator(names_array)
   speakers_array = []
-  name_array.each do |name|
+  names_array.each do |name|
     speakers_array << badge_maker(name)
   end
   speakers_array
 end
 
-def assign_rooms(name_array)
+def assign_rooms(names_array)
   room_assignment = []
-  name_array.each_with_index do |name, index|
+  names_array.each_with_index do |name, index|
     room_assignment << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
   room_assignment
 end
 
-def printer(attendees)
-  attendees = []
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+def printer(names_array)
+  batch_badge_creator(names_array).each do |attendee|
+    puts "#{attendee}"
+  end
+  assign_rooms(names_array).each do |attendee|
+    puts "#{attendee}"
+  end
 end
